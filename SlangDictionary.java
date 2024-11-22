1bc0da3d9250327c8bbaeb41ca864dfbeb13dab1
nhatphuoc <vonhatphuoc32@gmail.com>
2024-11-22 13:28:01 +0700

    find slang errors in dictionary file

--- SlangDictionary.java
+++ SlangDictionary.java
@@ -8,20 +8,23 @@ public class SlangDictionary {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("slang.txt"));
             String line;
-            
+            int i=1;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split("`");
                 
                 if (parts.length == 2) {
                     slangMap.put(parts[0].trim(), parts[1].trim());
+                } else {
+                    System.out.println(i+": "+line);
                 }
+                i++;
             }
             
             reader.close();
             
-            for (Map.Entry<String, String> entry : slangMap.entrySet()) {
-                System.out.println("Slang: " + entry.getKey() + " - Meaning: " + entry.getValue());
-            }
+            // for (Map.Entry<String, String> entry : slangMap.entrySet()) {
+            //     System.out.println("Slang: " + entry.getKey() + " - Meaning: " + entry.getValue());
+            // }
             
         } catch (IOException e) {
             e.printStackTrace();
